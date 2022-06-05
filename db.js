a12e1c9355a60751242401d1a605b1f380755352
Patryk <pjazdzewski1990@gmail.com>
2022-06-05 09:29:06 +0200

    Add a dedicated class for Tabs

--- db.js
+++ db.js
@@ -1,6 +1,23 @@
 // holds functions related to obtaining and handling tab state
 
+// domain specific wrapper around https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/Tab
+class Tab {
+    id;
+    title;
+    url;
+
+    constructor(id, title, url) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+    }
+}
+
 // get new tabs info
 export function refreshTabsState() {
-  return browser.tabs.query({currentWindow: true});
-}
+  return browser.tabs.query({currentWindow: true}).then(tabs => {
+    return tabs.map(tab => {
+        return new Tab(tab.id, tab.title, tab.url);
+    });
+  });
+};
