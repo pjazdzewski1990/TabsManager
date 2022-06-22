3c624af7eb232327d53799842f7bc6f7d43628d1
Patryk <pjazdzewski1990@gmail.com>
2022-06-22 20:38:03 +0200

    Basic language icons

--- src/ui.js
+++ src/ui.js
@@ -13,13 +13,28 @@ export function listTabs(tabs) {
     // prepare elements
     let currentTabs = document.createDocumentFragment();
     for (let tab of tabs) {
-        const tabLink = document.createElement('a');
+        const langIcon = document.createElement('img');
+        langIcon.alt = tab.languageCode;
+        // in case of trouble translating - assume en
+        // kudos to https://anandchowdhary.github.io/language-icons/
+        // and https://flagicons.lipis.dev/
+        let langLink = ("https://unpkg.com/language-icons/icons/" + tab.languageCode + ".svg");
+        if(tab.languageCode.toLowerCase() === "later") {
+            langLink = "https://raw.githubusercontent.com/lipis/flag-icons/main/flags/1x1/un.svg";
+        }
+        if(tab.languageCode.toLowerCase() === "unknown") {
+            langLink = "https://raw.githubusercontent.com/lipis/flag-icons/main/flags/1x1/xx.svg";
+        }
+        langIcon.src = langLink;
+        langIcon.classList.add('lang-icon');
 
+        const tabLink = document.createElement('a');
         tabLink.textContent = ("#" + tab.id + " " + tab.title);
         tabLink.setAttribute('href', tab.id);
         tabLink.classList.add('switch-tabs');
 
         const newListElem = document.createElement('li');
+        newListElem.appendChild(langIcon);
         newListElem.appendChild(tabLink);
         currentTabs.appendChild(newListElem);
     }
