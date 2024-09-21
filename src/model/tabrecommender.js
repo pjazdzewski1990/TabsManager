48af5ab5d7c3b7996bc3bfa011191765c5d97337
Patryk <pjazdzewski1990@gmail.com>
2024-09-21 16:16:49 +0200

    improve tab recommender

--- src/model/tabrecommender.js
+++ src/model/tabrecommender.js
@@ -11,22 +11,27 @@ export class SameWordsTabRecommender {
     return safeString.trim().split(/[^A-Za-z]/).filter((it) => it.length > 3).map((it) => it.toLowerCase());
   }
 
-  // from the given tabs selects one that is most similar to the one given
+  /**
+   * From the given tabs selects one that is most similar to the one given
+   * @param {EnrichedTab} similarTo Search for a tab silimar to this one
+   * @param {Array<EnrichedTab>} all Search among this list of tabs
+   * @returns First tab from the list that is similar enough to the given one, or the first one if nothing fits
+   */
   recommend(similarTo, all) {
-    console.log('recommend(similarTo, all)', similarTo);
-    console.log('recommend(similarTo, all)', all);
+    // console.log('recommend(similarTo, all)', similarTo);
+    // console.log('recommend(similarTo, all)', all);
     if (similarTo) {
       const similarToWords = this.#normalizeString(similarTo.title);
       const similar = all.find((tab) => {
         const tabWords = this.#normalizeString(tab.title);
         const overlap = similarToWords.filter((val) => tabWords.indexOf(val) !== -1);
-        // console.log(`${similarTo.title} <==> ${tab.title}`, overlap);
+        console.log(`${similarTo.title} <==> ${tab.title}`, overlap);
         // end if we found enough words
         return overlap.length >= this.wordThreshold;
       });
       return similar || all[0];
     }
-    // return anything if undefined or there's not match
+    // return something if undefined or there's no match
     return all[0];
   }
 }
--- tests/unit/tabrecommender.test.js
+++ tests/unit/tabrecommender.test.js
@@ -1,13 +1,16 @@
 import {SameWordsTabRecommender} from '../../src/model/tabrecommender.js';
 
+//TODO: use dummy tab objects
+// Note: order does matter as recommender takes the first one matching
+const tabsUnderTest = [
+    {title: "LondonTokyoChicago", url: ""}, // string is split by space, not characters
+    {title: "Paris", url: ""},
+    {title: "LONDON", url: ""},
+    {title: "Athens London Tokyo Chicago", url: ""},
+];
+
 test('SameWordsTabRecommender(wordThreshold=1) finds similar tabs based on similarity of titles', () => {
     //given some tabs
-    const tabsUnderTest = [
-        {id: 4, title: "LondonTokyoChicago", url: ""}, // string is split by space, not characters
-        {id: 1, title: "Paris", url: ""},
-        {id: 2, title: "LONDON", url: ""},
-        {id: 3, title: "Athens London Tokyo Chicago", url: ""},
-    ];
     //and recommender
     const recommenderFor1 = new SameWordsTabRecommender(1);
     //when we recommend we should get a similar tab
@@ -22,20 +25,16 @@ test('SameWordsTabRecommender(wordThreshold=1) finds similar tabs based on simil
     expect(recommenderFor1.recommend({title: "Cairo"}, [])).toEqual(undefined);
 });
 
-test('SameWordsTabRecommender(wordThreshold=1) finds similar tabs based on similarity of titles', () => {
+test('SameWordsTabRecommender(wordThreshold=2) finds similar tabs based on similarity of titles', () => {
     //given some tabs
-    const tabsUnderTest = [
-        {id: 1, title: "Paris", url: ""},
-        {id: 4, title: "LondonTokyoChicago", url: ""}, // string is split by space, not characters
-        {id: 2, title: "LONDON", url: ""},
-        {id: 3, title: "Athens London Tokyo Chicago", url: ""},
-    ];
     //and recommender
     const recommenderFor2 = new SameWordsTabRecommender(2);
-    //when we recommend we should get 1st elem as the query is too short
-    expect(recommenderFor2.recommend({title: "London"}, tabsUnderTest).title).toEqual("Paris");
+    //when we recommend we should get 1st elem as the query is too short - we return the first
+    expect(recommenderFor2.recommend({title: "London"}, tabsUnderTest).title).toEqual("LondonTokyoChicago");
     //when we recommend we should get first tab that matches at least 2 words
     expect(recommenderFor2.recommend({title: "Chicago Tokyo"}, tabsUnderTest).title).toEqual("Athens London Tokyo Chicago");
+    //when we recommend we should get first tab that matches at least 2 words - order doesn't matter
+    expect(recommenderFor2.recommend({title: "Tokyo Chicago"}, tabsUnderTest).title).toEqual("Athens London Tokyo Chicago");
 });
 
 //TODO: find a way to test FF APIs as mocking everything is not viable in the the long run
