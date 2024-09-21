1872942f9425111e14cf778d16fd7ad8bfbf80c3
Patryk <pjazdzewski1990@gmail.com>
2024-09-21 15:56:11 +0200

    add more search tests + normalize language too

--- src/model/search.js
+++ src/model/search.js
@@ -1,10 +1,19 @@
 /**
+ * Given a string input alignes the case and spaces
+ * @param {String} str String to normalize
+ * @returns Normalized value that can be reliably used in search
+ */
+function normalizeStringForSearch(str) {
+    return str.toLowerCase().trim();
+}
+
+/**
  * Turns the provided EnrichedTab into a searcheable string
  * @param {EnrichedTab} tab Tab info to be converted
  * @returns {string} A consistent, searcheable string
  */
 function tabToSearchString(tab) {
-  return (`${tab.title} l:${tab.languageCode}`).toLowerCase();
+  return (`${tab.title} l:${normalizeStringForSearch(tab.languageCode)}`).toLowerCase();
 }
 
 /**
@@ -15,12 +24,12 @@ function tabToSearchString(tab) {
  */
 export function filterTabState(tabs, stringQuery) {
   // input like "foo   BAR" becomes a "foo|bar" regexp
-  const searchExpression = stringQuery.toLowerCase().replace(/\s+/g, '|');
+  const searchExpression = normalizeStringForSearch(stringQuery).replace(/\s+/g, '|');
 
   return tabs.filter((tab) => {
     const searchContent = tabToSearchString(tab);
     const wasFound = new RegExp(searchExpression).test(searchContent);
-    // console.log("Testing " + searchContent + " for " + searchExpression + " => " + wasFound);
+    console.log("Testing " + searchContent + " for " + searchExpression + " => " + wasFound);
     return wasFound;
   });
 }
--- tests/unit/search.test.js
+++ tests/unit/search.test.js
@@ -9,22 +9,48 @@ class TestTabProvider {
     }
 }
 
+const tabsUnderTest = [
+    {id: 1, title: "Test1", languageCode: "en", url: "http://example.com/1"},
+    {id: 2, title: "TEST 2", languageCode: "de",  url: "http://example.com/2"},
+    {id: 3, title: "test 3", languageCode: "es",  url: "http://example.com/3"},
+    {id: 4, title: "  Test4 ", languageCode: " EN ",  url: "http://example.com/4"},
+    {id: 5, title: "  Testtttt 5", languageCode: " EN ",  url: "http://example.com/4"},
+    {id: 10, title: "Foo", languageCode: "en",  url: "http://example.com/foo"},
+    {id: 11, title: "Bar", languageCode: "pl",  url: "http://example.com/bar"}
+];
+
 test('filters tabs based on the title', () => {
     //given some tabs
-    const tabsUnderTest = [
-        {id: 1, title: "Test1", url: "http://example.com/1"},
-        {id: 2, title: "TEST 2", url: "http://example.com/2"},
-        {id: 3, title: "test 3", url: "http://example.com/3"},
-        {id: 4, title: "  Test4", url: "http://example.com/4"},
-        {id: 5, title: "Foo", url: "http://example.com/foo"},
-        {id: 6, title: "Bar", url: "http://example.com/bar"}
-    ];
     const provider = new TestTabProvider(tabsUnderTest);
     //when we filter by Test
     const resultPromise = provider.provide().then(tabs => filterTabState(tabs, "Test"));
     //then we expect the results to be filtered by title
     return resultPromise.then(result => {
-        expect(result.length).toEqual(4);
-        expect(result.map(t => t.id)).toEqual([1,2,3,4]);
+        const expected = [1,2,3,4,5];
+        expect(result.map(t => t.id)).toEqual(expected);
+    });
+});
+
+test('filters tabs based on the title and language code', () => {
+    //given some tabs
+    const provider = new TestTabProvider(tabsUnderTest);
+    //when we filter by: Test OR language is pl
+    const resultPromise = provider.provide().then(tabs => filterTabState(tabs, "l:pl"));
+    //then we expect the results to be filtered by language
+    return resultPromise.then(result => {
+        const expected = [11];
+        expect(result.map(t => t.id)).toEqual(expected);
+    });
+});
+
+test('filters tabs based on the title and language code', () => {
+    //given some tabs
+    const provider = new TestTabProvider(tabsUnderTest);
+    //when we filter by: Foo OR language is en 
+    const resultPromise = provider.provide().then(tabs => filterTabState(tabs, "Foo l:en"));
+    //then we expect the results to be filtered
+    return resultPromise.then(result => {
+        const expected = [1,4,5,10];
+        expect(result.map(t => t.id)).toEqual(expected);
     });
 });
